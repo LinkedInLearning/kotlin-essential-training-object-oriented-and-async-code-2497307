7cde9ab12fd3250f49eca7f0c94df7c64eb9ad26
linkedin <n8ebel@goobar.dev>
2022-04-29 22:46:59 -0600

    02_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 
+const val DEFAULT_CLICK_DELAY = 500
+
+private fun log(error: Throwable) = println(error)
 
 fun main() {
-
+    millisForHours(5)
 }
---
+++ project/src/main/kotlin/TimeUtils.kt
@@ -0,0 +1,4 @@
+
+private val hourInMillis = 60 * 60 * 1000
+
+internal fun millisForHours(hours: Int) = hours * hourInMillis
