4973d96f5d25da93dbf6b84c445e7902a739de15
linkedin <n8ebel@goobar.dev>
2022-04-29 22:37:44 -0600

    Prep for 01_03

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,27 +1,5 @@
 
-interface StringProvider {
-
-    val placeholder: String
-
-    fun getString(id: Int): String = id.toString()
-}
-
-interface ResourceProvider : StringProvider {
-    fun getDimension(id: Int): Long = id.toLong()
-}
-
-class DefaultResourceProvider : ResourceProvider {
-
-    override val placeholder: String = "<placeholder>"
-}
 
 fun main() {
-    val someVariable: Any? = null
-    if (someVariable is StringProvider) {
-        println("The variable was a StringProvider")
-    }
-
-    var stringProvider: StringProvider = DefaultResourceProvider()
-    println(stringProvider.getString(5))
-    println(stringProvider.placeholder)
+    
 }
