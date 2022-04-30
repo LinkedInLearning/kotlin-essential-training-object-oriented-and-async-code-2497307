c51170e35ad09af8d59663d936a0191f16e9ca6c
linkedin <n8ebel@goobar.dev>
2022-04-29 22:46:27 -0600

    Prep for 02_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,29 +1,5 @@
-import kotlin.random.Random
 
-// Use getRandomString() to write a function named getString()
-// getString() should call getRandomString() and return StringResult
-// calling getString() should never crash
-// errors should be wrapped with StringResult.Failure
+
 fun main() {
-    when (val result = getString()) {
-        is StringResult.Error -> println("There was an error")
-        is StringResult.Success -> println(result.value)
-    }
-}
 
-fun getString(): StringResult {
-    return try {
-        StringResult.Success(getRandomString())
-    } catch (error: java.lang.IllegalStateException) {
-        StringResult.Error(error)
-    }
-}
-
-fun getRandomString(): String {
-    val rand = Random.nextInt(10)
-    return if (rand > 5) {
-        throw IllegalStateException()
-    } else {
-        rand.toString()
-    }
 }
