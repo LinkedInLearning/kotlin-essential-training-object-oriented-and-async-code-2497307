95eea833666a11728871215a465692d0722f5877
linkedin <n8ebel@goobar.dev>
2022-04-29 22:53:09 -0600

    02_05

--- project/src/main/kotlin/ListUtils.kt
+++ project/src/main/kotlin/ListUtils.kt
@@ -1,19 +1,14 @@
-// Refactor the methods to top-level extension functions
-// Once the functions are converted, the ListUtils class shouldn't be needed
-class ListUtils {
-    companion object {
-        fun <T> nonEmptyList(vararg items: T): List<T> {
-            check(items.isNotEmpty()) {
-                "NonEmptyList must have at least one item"
-            }
-            return items.toList()
-        }
 
-        fun <T> setToNonEmptyList(set: Set<T>): List<T> {
-            check(set.isNotEmpty()) {
-                "NonEmptyList must have at least one item"
-            }
-            return set.toList()
-        }
+fun <T> Array<T>.nonEmptyList(): List<T> {
+    check(isNotEmpty()) {
+        "NonEmptyList must have at least one item"
     }
+    return toList()
+}
+
+fun <T> Set<T>.toNonEmptyList(): List<T> {
+    check(isNotEmpty()) {
+        "NonEmptyList must have at least one item"
+    }
+    return toList()
 }
--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 
 
 fun main() {
-    ListUtils.nonEmptyList(arrayOf(1, 2, 3))
-    ListUtils.setToNonEmptyList(setOf("a", "b", "c"))
+
+    arrayOf(1, 2, 3).nonEmptyList()
+    setOf("a", "b", "c").toNonEmptyList()
+
+//    ListUtils.nonEmptyList(arrayOf(1, 2, 3))
+//    ListUtils.setToNonEmptyList(setOf("a", "b", "c"))
 }
