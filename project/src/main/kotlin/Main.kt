f9bad9145b01f15c1e7e3ba79870fae683d98864
linkedin <n8ebel@goobar.dev>
2022-04-29 22:57:03 -0600

    Prep for 03_04

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,7 +1,20 @@
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+private suspend fun loadItemsFromDB(): List<String> {
+    delay(5000)
+    return listOf("Kotlin", "Java", "C#")
+}
+
+private suspend fun loadItemsFromNetwork(): List<String> {
+    delay(5000)
+    return listOf("Rust", "Python", "C")
+}
+
+// Use async() to parallelize the execution of the two provided suspend functions
+// Use await() to get results of both async() calls
+// Convert the results into a unified list and print out to the console
 fun main(): Unit = runBlocking {
-    launch { println("Hello Coroutines") }
+
 }
