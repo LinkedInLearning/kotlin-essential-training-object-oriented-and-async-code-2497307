9db85b17aa0143951fcdd18f0c90656795e42d0b
linkedin <n8ebel@goobar.dev>
2022-04-29 22:53:50 -0600

    Prep for 03_01

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 
 
 fun main() {
-
-    arrayOf(1, 2, 3).nonEmptyList()
-    setOf("a", "b", "c").toNonEmptyList()
-
-//    ListUtils.nonEmptyList(arrayOf(1, 2, 3))
-//    ListUtils.setToNonEmptyList(setOf("a", "b", "c"))
+    
 }
