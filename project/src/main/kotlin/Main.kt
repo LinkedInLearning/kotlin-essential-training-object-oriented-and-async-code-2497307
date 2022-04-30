6ad510cbc9b576e15dd97ed338fec285253d41cf
linkedin <n8ebel@goobar.dev>
2022-04-29 22:54:49 -0600

    Prep for 03_02

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,33 +1,4 @@
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
-
-class CustomThread : Thread("CustomThread") {
-    override fun run() {
-        super.run()
-        println(Thread.currentThread().name)
-        println("CustomThread.run()")
-    }
-}
-
-class CustomRunnable : Runnable {
-    override fun run() {
-        println(Thread.currentThread().name)
-        println("CustomRunnable.run()")
-    }
-}
-
-val executor = Executors.newSingleThreadExecutor()
-val multiThreadExecutor = Executors.newFixedThreadPool(3)
 
 fun main() {
-    println(Thread.currentThread().name)
-    //CustomThread().start()
 
-//    Thread(CustomRunnable()).start()
-
-//    executor.submit(CustomRunnable())
-
-    for (i in 0..10) {
-        multiThreadExecutor.submit(CustomRunnable())
-    }
 }
