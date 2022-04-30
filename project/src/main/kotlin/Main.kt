1faf3cd3246021dba76a369b2366fc50bcf6bdfd
linkedin <n8ebel@goobar.dev>
2022-04-29 22:42:21 -0600

    Prep for 01_07

--- project/src/main/kotlin/Main.kt
+++ project/src/main/kotlin/Main.kt
@@ -1,17 +1,5 @@
 
-fun render(state: UiState) = when (state) {
-    is UiState.Error -> println("Error")
-    is UiState.Loaded -> println("Loaded: ${state.title}")
-    UiState.Loading -> println("Loading")
-}
 
 fun main() {
-    var state: UiState = UiState.Loading
-    render(state)
-
-    state = UiState.Loaded("Kotlin", "Is cool!")
-    render(state)
-
-    state = UiState.Error(IllegalStateException())
-    render(state)
+    
 }
